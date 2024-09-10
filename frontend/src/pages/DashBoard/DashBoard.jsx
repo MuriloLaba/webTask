d1b7472e59e3092e96bdd3004588142cf62bdda9
Murilo Laba <84825163+MuriloLaba@users.noreply.github.com>
2024-09-10 19:24:59 -0300

    Implementendo funcionalidades novas

--- frontend/src/pages/DashBoard/DashBoard.jsx
+++ frontend/src/pages/DashBoard/DashBoard.jsx
@@ -5,7 +5,13 @@ import Task from "./components/Task";
 import AddTaskModal from "./components/AddTaskModal";
 
 export function DashBoard() {
-  const { addTask, renderTasksByStatus, handleDrop, allowDrop, users } = useDashBoard();
+  const {
+    handleAddTask,
+    renderTasksByStatus,
+    handleDrop,
+    allowDrop,
+    users,
+  } = useDashBoard();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpen = () => setIsModalOpen(!isModalOpen);
@@ -13,38 +19,49 @@ export function DashBoard() {
   return (
     <div className="mt-8 flex flex-col size-full">
       <Card className="bg-black">
-        
         <CardHeader variant="gradient" color="indigo" className="relative mb-3 p-2">
           <div className="flex items-center justify-between mx-4">
-            <Typography variant="h3" color="white" className="select-none">Tarefas</Typography>
-            <Button color="green" onClick={handleOpen}>Adicionar</Button>
+            <Typography variant="h3" color="white" className="select-none">
+              Tarefas
+            </Typography>
+            <Button color="green" onClick={handleOpen}>
+              Adicionar
+            </Button>
           </div>
         </CardHeader>
 
         <CardBody className="flex px-4 pt-0 pb-2 gap-4 overflow-hidden h-[84vh]">
           <div className="flex w-full justify-around">
-            <div className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
-              onDrop={(e) => handleDrop(e, 0)} onDragOver={allowDrop}>
+            <div
+              className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
+              onDrop={(e) => handleDrop(e, 0)}
+              onDragOver={allowDrop}
+            >
               <Typography variant="h5" color="white" className="self-center">Pendentes</Typography>
-              {renderTasksByStatus(0, Task)}
+              {renderTasksByStatus(0, Task)} 
             </div>
 
-            <div className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
-              onDrop={(e) => handleDrop(e, 1)} onDragOver={allowDrop}>
+            <div
+              className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
+              onDrop={(e) => handleDrop(e, 1)}
+              onDragOver={allowDrop}
+            >
               <Typography variant="h5" color="white" className="self-center">Em Andamento</Typography>
-              {renderTasksByStatus(1, Task)}
+              {renderTasksByStatus(1, Task)} 
             </div>
 
-            <div className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
-              onDrop={(e) => handleDrop(e, 2)} onDragOver={allowDrop}>
+            <div
+              className="w-1/3 bg-black rounded-xl flex flex-col p-4 overflow-y-scroll no-scrollbar"
+              onDrop={(e) => handleDrop(e, 2)}
+              onDragOver={allowDrop}
+            >
               <Typography variant="h5" color="white" className="self-center">Concluídas</Typography>
-              {renderTasksByStatus(2, Task)}
+              {renderTasksByStatus(2, Task)} 
             </div>
           </div>
         </CardBody>
-
       </Card>
-      <AddTaskModal open={isModalOpen} handleOpen={handleOpen} addTask={addTask} users={users} />
+      <AddTaskModal open={isModalOpen} handleOpen={handleOpen} addTask={handleAddTask} users={users} />
     </div>
   );
 }
